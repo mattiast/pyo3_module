f6f32f6d0047528bf252025103c7abb0985fb5e7
Matti Astrand <mattiastr@gmail.com>
2020-04-04 17:36:07 +0100

    Add a function to mutate a numpy array in place

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     version="0.1.0",
     rust_extensions=[RustExtension("sample_module", binding=Binding.PyO3)],
     setup_requires=["setuptools-rust>=0.10.1", "wheel"],
+    install_requires=["numpy"],
 )
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn};
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -5,6 +7,19 @@ use pyo3::wrap_pyfunction;
 /// add 5 to a nonnegative integer
 pub fn add5(x: u32) -> PyResult<u32> {
     Ok(x + 5)
+}
+
+#[pyfunction]
+pub fn cumsum_inplace(x: &PyArrayDyn<f64>) -> PyResult<()> {
+    let mut x = x.as_array_mut();
+
+    let mut s: f64 = 0.0;
+    for xi in x.iter_mut() {
+        s += *xi;
+        *xi = s;
+    }
+
+    Ok(())
 }
 
 #[pyclass]
@@ -17,7 +32,7 @@ struct Juttu {
 impl Juttu {
     #[new]
     fn new(x: i32, y: bool) -> Self {
-        let juttu = Juttu {x, y};
+        let juttu = Juttu { x, y };
         juttu
     }
 
@@ -30,6 +45,7 @@ impl Juttu {
 #[pymodule]
 fn sample_module(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(add5))?;
+    m.add_wrapped(wrap_pyfunction!(cumsum_inplace))?;
     m.add_class::<Juttu>()?;
 
     Ok(())
